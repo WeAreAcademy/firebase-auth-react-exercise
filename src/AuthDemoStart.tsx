23f649f1d9215fd79f0c0f8dfa2472490df7dcac
nbogie <neillbogie@googlemail.com>
2022-05-26 22:14:23 +0100

    finished wkshp 1+2

--- src/AuthDemoStart.tsx
+++ src/AuthDemoStart.tsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { auth, googleAuthProvider } from "./configureFirebase";
+import { signInWithPopup, User } from "firebase/auth"
 
 export function AuthDemoStart(): JSX.Element {
     const [lastAPIReply, setLastAPIReply] = useState<string>("");
-
+    const [user, setUser] = useState<User | null>(null);
 
     async function handleFetchTimeClicked() {
         const reply = await axios.get("http://localhost:4000/");
@@ -12,17 +14,34 @@ export function AuthDemoStart(): JSX.Element {
 
     async function handleFetchWisdomClicked() {
         //This SHOULD be hard to get, eventually.
-        const reply = await axios.get("http://localhost:4000/wisdom");
+        if (!user) {
+            console.log("not logged in ")
+            return;
+        }
+        const token = await user.getIdToken()
+        const config = { headers: { "Authorization": "Bearer " + token } };
+        const reply = await axios.get("http://localhost:4000/wisdom", config);
         setLastAPIReply(reply.data);
+    }
+
+    async function handleSignInClicked() {
+        const userCredential = await signInWithPopup(auth, googleAuthProvider);
+        const retrievedUser: User = userCredential.user;
+        setUser(retrievedUser);
+    }
+
+    async function handleSignOutClicked() {
+        auth.signOut();
+        setUser(null);
     }
 
     return (
         <div>
             <h2>Auth Demo</h2>
 
-            <button onClick={() => alert("not implemented")}>Sign in</button>
-            <button onClick={() => alert("not implemented")}>Sign out</button>
-
+            <button onClick={handleSignInClicked}>Sign in</button>
+            <button onClick={handleSignOutClicked}>Sign out</button>
+            <div>Signed in as: {user?.displayName}</div>
             <hr />
             <h3>Talk to the API</h3>
             <button onClick={handleFetchTimeClicked}>Fetch Time</button>
